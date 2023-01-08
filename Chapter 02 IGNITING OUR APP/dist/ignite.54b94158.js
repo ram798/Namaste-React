0d61d76c0cd35410c8db5f658a108fa9ab3025fd
Siva Ram <87913063+ram798@users.noreply.github.com>
2023-01-08 14:07:49 +0530

    IGNITING our App

--- Chapter 02 IGNITING OUR APP/dist/ignite.54b94158.js
+++ Chapter 02 IGNITING OUR APP/dist/ignite.54b94158.js
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-
 const inception = document.createElement("h1");
 inception.innerHTML = "This is my HEllo WoRlD!!";
 const branch = document.getElementById("fight");
 fight.appendChild(inception);
-
-const current = React.createElement("h1", {id:"current",}, "My WOrld is on the way");
-const steve = React.createElement("div", {id:"steve",}, [current]);
+const current = React.createElement("h1", {
+    id: "current"
+}, "My WOrld is on the way");
+const steve = React.createElement("div", {
+    id: "steve"
+}, [
+    current
+]);
 const flight = ReactDOM.createRoot(document.getElementById("flight"));
 flight.render(steve);
+
+//# sourceMappingURL=ignite.54b94158.js.map
